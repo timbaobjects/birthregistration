b8e1f45bf612a60dec1bba60228323e818f67431
Tim Akinbo <takinbo@timbaobjects.com>
2016-10-10 22:46:56 +0100

    refactoring of the br app handler

--- br/app.py
+++ br/app.py
@@ -2,12 +2,14 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 import calendar
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
+from django.utils.translation import ugettext as _
 import logging
+import parsley
 import re
 # requires python-dateutil to work
 try:
-    from dateutil.parser import *
+    from dateutil.parser import parse
     from dateutil.relativedelta import relativedelta
 except ImportError:
     raise ImportError('python-dateutil is required for this app to work.')
@@ -15,39 +17,45 @@ from rapidsms.apps.base import AppBase
 from models import BirthRegistration
 from reporters.models import PersistantConnection, Reporter, Role
 from locations.models import Location
-from .grammars import parse_date, parse_report
 
-MAX_REPORT_WINDOW = 90 * 3600 * 24  # reports older than 90 days from the day of submission will be rejected
+MAX_REPORT_WINDOW = 90 * 24 * 3600  # reports older than 90 days from the day of submission will be rejected
 logger = logging.getLogger(__name__)
+help_grammar = parsley.makeGrammar('''
+    help = 'br' ws 'help' ws <letter*>:selector -> (selector)
+''', {})
+br_grammar = parsley.makeGrammar('''
+    parameter = <digit+>:datum ws -> datum
+    male_report = ('m' | 'b'):gender ws <parameter{1,4}>:data -> ('m', data.strip().split(' '))
+    female_report = ('f' | 'g'):gender ws <parameter{1,4}>:data -> ('f', data.strip().split(' '))
+    report = 'br' ws 'report' ws (male_report | female_report){1,2}:gender_report -> (gender_report)
+    register = 'br' ws 'register' ws <digit+>:location_code ws <letterOrDigit+>:role ws <anything*>:name -> (location_code, role, name)
+''', {})
 
 
 class BirthRegistrationApp(AppBase):
-
-    error_msgs = {
-        'invalid_location': "Sorry, I don't know any location with the code: {}. You sent: {}",
-        'invalid_role': "Unknown role code: {}. You sent: {}",
-        'unauthorized_reporter': 'Please register your number with RapidSMS before sending this report',
-        'unauthorized_role': 'You are not authorized to send this report.',
-        'invalid_date': 'An invalid date was found in your report. Date format should be DD/MM/YYYY. You sent: {}',
-        'invalid_report': 'I don\'t understand the report you sent. You sent: {}',
-        }
-    response_msgs = {
-        'help': "['report', 'register']",
-        'registered': 'Hello {name}! You are now registered as {role} at {location_name} {location_type}.',
-        'already_registered': 'Hello again {name}! You are already registered as a {role} at {location_name}.',
-        'report': "{date} BR report: Girls below 1={g1}, 1-4={g4}, 5-9={g9}, 10-17={g18}, Boys below 1={b1}, 1-4={b4}, 5-9={b9}, 10-17={b18} for {location}",
+    error_messages = {
+        'invalid_location': _("You sent an incorrect location code: %(location_code)s. You sent: %(text)s"),
+        'invalid_role': _("Unknown role code: %(role_code)s. You sent: %(text)s"),
+        'unauthorized_reporter': _("Please register your number with RapidSMS before sending this report."),
+        'unauthorized_role': _('You are not authorized to send this report.'),
+        'invalid_date': _("You used an incorrect date in your report. Your date should be in the format DD/MM/YYYY. You sent: %(text)s"),
+        'early_report': _("You used an incorrect date in your report. You can't send a report earlier than %(early_date)s. You sent: %(text)s"),
+        'future_report': _("You used an incorrect date in your report. You can't send a report for a date in the future. You sent: %(text)s"),
+        'invalid_message': _("Your message is incorrect. Please send BR HELP for help on sending a report. You sent: %(text)s"),
+    }
+    help_messages = {
+        'general': _("For help on BR reports, reply: BR HELP REPORT. For help on registration, reply: BR HELP REGISTER"),
+        'report': _("To send BR report, send BR REPORT B #under_1 #1_to_4 #5_to_9 #10_to_17 G #under_1 #1_to_4 #5_to_9 #10_to_17"),
+        'register': _("To register, send BR REGISTER location_code BR full_name"),
+    }
+    response_messages = {
+        'registered': _("Hello %(name)s! You are now registered as %(role)s at %(location_name)s %(location_type)s."),
+        'already_registered': _("Hello again %(name)s! You are already registered as a %(role)s at %(location_name)s."),
+        'report': _("Thank you for your BR report. Girls <1=%(girls_below1)d, 1-4=%(girls_1to4)d, 5-9=%(girls_5to9)d, 10+=%(girls_10to18)d; Boys <1=%(boys_below1)d, 1-4=%(boys_1to4)d, 5-9=%(boys_5to9)d, 10+=%(boys_10to18)d sent %(date)s for %(location)s"),
     }
 
     prefix = re.compile('^\s*(?:br)(?:[\s,;:]*)(?P<parts>.*)$', re.IGNORECASE)
-    register_pattern = re.compile('^register (?P<location_code>\d+) (?P<role>[a-z0-9_]+) (?P<name>.*)$', re.IGNORECASE)
-    report_pattern = re.compile('^report (?P<gender_data>.*)$', re.IGNORECASE)
-    date_pattern = re.compile(r"(?P<date>(\d+[\/\-]\d+|\d+[\/\-]\d+[\/\-]\d+))$")
-
-    # the reason we're using two regexes instead of one is because we want to
-    # know when someone has actually entered an invalid date. if we use just
-    # one, a last value like '12-12' would assign the current date to the
-    # report, and that is not what we want.
-    strict_date_pattern = re.compile(r"(?P<date>(\d{2}[\/\-]\d{2}[\/\-]\d{4}))$")
+    date_expression = re.compile('(?P<text>.+)\s(?P<date>\d{1,2}[\/\-]\d{1,2}([\/\-]\d{2,4})?)?$')
 
     def parse(self, msg):
         conn = PersistantConnection.from_message(msg)
@@ -59,75 +67,66 @@ class BirthRegistrationApp(AppBase):
         else:
             msg.persistance_dict = {"connection": msg.persistant_connection}
 
-        # log, whether we know who the sender is or not
-        if msg.reporter:
-            logger.info("Identified: %s as %r" % (conn, msg.reporter))
-        else:
-            logger.info("Unidentified: %s" % (conn))
-
-        # update last_seen, which automatically
-        # populates the same property
         conn.seen()
-
-    def parse_date(self, message):
-        '''The purpose of this method is to look for any strings resembling
-        a date string eg. 7/12/2009, 7/12, 7-12-2009, 7-12, etc and use that
-        to configure the date of the message. This is essentially to cater for
-        reports that may come in late.'''
-        msg_date = None
-        try:
-            msg_date = parse_date(message.text)
-        except ValueError:
-            message.respond(self.error_msgs['invalid_date'].format(message.text))
-            return False
-
-        moment = datetime.now()
-        if not msg_date:
-            if ('-' not in message.text) and ('/' not in message.text):
-                # sender never tried to specify a date
-                msg_date = moment
-            else:
-                # sender tried to specify a date, but it was invalid
-                message.respond(self.error_msgs['invalid_date'].format(message.text))
-                return False
-        
-        if msg_date <= moment:
-            date_diff = moment - msg_date
-            if abs(date_diff.total_seconds()) > MAX_REPORT_WINDOW:
-                message.respond(self.error_msgs['invalid_date'].format(message.text))
-                return False
-
-            logger.info("setting message date to %s" % msg_date)
-            message.datetime = self._classify_date(msg_date)
-
-            # Remove date from message
-            if ('-' in message.text) or ('/' in message.text) or ('\\' in message.text):
-                logger.info("previous message: %s" % message.text.strip())
-                msgtxt = " ".join(re.split("\s+", message.text.strip())[0:-1])
-                message.text = msgtxt
-                logger.info("new message: %s" % message.text)
-            return True
-        else:
-            message.respond(self.error_msgs['invalid_date'].format(message.text))
-            return False
 
     def handle(self, message):
         try:
-            match = self.prefix.match(message.text)
-            if match:
+            text_message = message.text.lower().strip()
+            if text_message.startswith('br'):
+                now = datetime.now()
                 # parse and extract any specified dates
-                status = self.parse_date(message)
-                if not status:
-                    # invalid date message has been sent
+                search_query = re.search(self.date_expression, text_message)
+                if search_query:
+                    message_parts = search_query.groups()
+                    # a date string was found in the text
+                    try:
+                        message_date = parse(message_parts[1], dayfirst=True)
+                        text_message = message_parts[0]
+                    except ValueError:
+                        message.respond(self.error_messages['invalid_date'] % dict(text=message.text))
+                        return True
+                else:
+                    message_date = now
+
+                if message_date <= now:
+                    time_diff = now - message_date
+                    oldest_date = now - timedelta(0, MAX_REPORT_WINDOW)
+                    if time_diff.total_seconds() > MAX_REPORT_WINDOW:
+                        message.respond(self.error_messages['early_report'] % dict(text=message.text, early_date=oldest_date.strftime('%d/%m/%Y')))
+                        return True
+
+                    logger.info('setting message date to %s' % message_date)
+                    message.datetime = self._classify_date(message_date)
+                else:
+                    message.respond(self.error_messages['future_report'] % dict(text=message.text))
                     return True
 
-                m = self.register_pattern.match(match.group('parts'))
-                if m:
-                    self.register(message, **m.groupdict())
-                    return True
-                m = self.report_pattern.match(match.group('parts'))
-                if m:
-                    self.report(message, **m.groupdict())
+                try:
+                    if help_grammar(text_message).help() != None:
+                        self.help(message)
+                        return True
+                except parsley.ParseError:
+                    pass
+
+                try:
+                    registration_data = br_grammar(text_message).register()
+                    if registration_data:
+                        self.register(
+                            message,
+                            registration_data[0],
+                            registration_data[1],
+                            registration_data[2].upper())
+                        return True
+                except parsley.ParseError:
+                    pass
+
+                try:
+                    br_data = br_grammar(text_message).report()
+                    if br_data:
+                        self.report(message, br_data)
+                        return True
+                except parsley.ParseError:
+                    message.respond(self.error_messages['invalid_message'] % dict(text=message.text))
                     return True
 
                 self.help(message)
@@ -136,7 +135,17 @@ class BirthRegistrationApp(AppBase):
             logger.error(e)
 
     def help(self, message):
-        message.respond(self.response_msgs['help'])
+        text_message = message.text.lower().strip()
+        try:
+            selector = help_grammar(text_message).help()
+            if selector == 'register':
+                return message.respond(self.help_messages['register'])
+            elif selector == 'report':
+                return message.respond(self.help_messages['report'])
+        except parsley.ParseError:
+            pass
+
+        return message.respond(self.help_messages['general'])
 
     def register(self, message, location_code, role, name=''):
         data = {}
@@ -147,7 +156,7 @@ class BirthRegistrationApp(AppBase):
             rep = Reporter(**data)
             conn = PersistantConnection.from_message(message)
             if Reporter.exists(rep, conn):
-                message.respond(self.response_msgs['already_registered'].format(
+                message.respond(self.response_messages['already_registered'] % dict(
                     name=rep.first_name,
                     role=rep.role,
                     location_name=rep.location))
@@ -157,33 +166,30 @@ class BirthRegistrationApp(AppBase):
             conn.reporter = rep
             conn.save()
 
-            message.respond(self.response_msgs['registered'].format(
-                            name=rep.first_name,
-                            role=rep.role,
-                            location_name=rep.location,
-                            location_type=rep.location.type))
+            message.respond(self.response_messages['registered'] % dict(
+                name=rep.first_name,
+                role=rep.role,
+                location_name=rep.location,
+                location_type=rep.location.type))
         except Role.DoesNotExist:
-            message.respond(self.error_msgs['invalid_role'].format(role, message.text))
+            message.respond(self.error_messages['invalid_role'] % dict(role_code=role, text=message.text))
         except Location.DoesNotExist:
-            message.respond(self.error_msgs['invalid_location'].format(location_code, message.text))
+            message.respond(self.error_messages['invalid_location'] % dict(location_code=location_code, text=message.text))
 
         return True
 
     def report(self, message, gender_data):
-        # report = self._parse_gender_data(gender_data)
-        report = parse_report(message.text)
-
-        if not report:
-            message.respond(self.error_msgs['invalid_report'].format(message.text))
-            return True
+        report = dict(gender_data)
+        for gender in ['m', 'f']:
+            report[gender] = map(lambda i: int(i), report[gender]) + ([0] * (4 - len(report[gender]))) if report[gender] else [0, 0, 0, 0]
 
         try:
             if not hasattr(message, 'reporter') or not message.reporter:
-                message.respond(self.error_msgs['unauthorized_reporter'])
+                message.respond(self.error_messages['unauthorized_reporter'])
                 return True
 
             if not message.reporter.role.code.lower() in ['br']:
-                message.respond(self.error_msgs['unauthorized_role'])
+                message.respond(self.error_messages['unauthorized_role'])
                 return True
 
             location = message.reporter.location
@@ -202,64 +208,33 @@ class BirthRegistrationApp(AppBase):
                 br.location = location
                 br.time = message.datetime
 
-            br.girls_below1 = report['female'].get(1)
-            br.girls_1to4 = report['female'].get(2)
-            br.girls_5to9 = report['female'].get(3)
-            br.girls_10to18 = report['female'].get(4)
-            br.boys_below1 = report['male'].get(1)
-            br.boys_1to4 = report['male'].get(2)
-            br.boys_5to9 = report['male'].get(3)
-            br.boys_10to18 = report['male'].get(4)
+            br.girls_below1 = report['f'][0]
+            br.girls_1to4   = report['f'][1]
+            br.girls_5to9   = report['f'][2]
+            br.girls_10to18 = report['f'][3]
+            br.boys_below1  = report['m'][0]
+            br.boys_1to4    = report['m'][1]
+            br.boys_5to9    = report['m'][2]
+            br.boys_10to18  = report['m'][3]
 
             br.save()
 
             # respond adequately
-            message.respond(self.response_msgs['report'].format(
+            message.respond(self.response_messages['report'] % dict(
                 location=location.name,
                 date=message.datetime.strftime('%d/%m/%Y'),
-                g1=br.girls_below1,
-                g4=br.girls_1to4,
-                g9=br.girls_5to9,
-                g18=br.girls_10to18,
-                b1=br.boys_below1,
-                b4=br.boys_1to4,
-                b9=br.boys_5to9,
-                b18=br.boys_10to18))
+                girls_below1=br.girls_below1,
+                girls_1to4=br.girls_1to4,
+                girls_5to9=br.girls_5to9,
+                girls_10to18=br.girls_10to18,
+                boys_below1=br.boys_below1,
+                boys_1to4=br.boys_1to4,
+                boys_5to9=br.boys_5to9,
+                boys_10to18=br.boys_10to18))
         except Exception, e:
             logger.debug(e)
 
         return True
-
-    def _parse_gender_data(self, s):
-        ''' parser for the gender data submitted
-        by the birth registrars
-        gender_data         ::= number
-        gender_male         ::= M|m|B|b
-        gender_female       ::= F|f|G|g
-        gender_submission   ::= (gender_male|gender_female) gender_data*
-        submitted_data      ::= gender_submission*'''
-
-        male_re = re.compile(r'^(m|b).*', re.I)
-        female_re = re.compile(r'^(f|g).*', re.I)
-
-        in_data = ''
-        gender_data = {'male': {}, 'female': {}}
-        counter = 0
-        gen = s.split()
-        for token in gen:
-            if male_re.match(token):
-                in_data = 'male'
-                counter = 1
-                continue
-            if female_re.match(token):
-                in_data = 'female'
-                counter = 1
-                continue
-            if token.isdigit() and in_data:
-                gender_data[in_data][counter] = int(token)
-                counter += 1
-
-        return gender_data
 
     def _classify_date(self, message_date):
         month_end = calendar.monthrange(message_date.year, message_date.month)[1]
