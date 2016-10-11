ce1da437c88b60bd69ec0d437a537585e8b2f13d
Oladipo Odumosu <dodumosu@timbaobjects.com>
2016-10-11 20:02:18 +0100

    Added processing of subkeywords

--- handlers/fuzzy.py
+++ handlers/fuzzy.py
@@ -5,11 +5,33 @@ from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
 
 class FuzzySubKeywordHandler(KeywordHandler):
     '''Keyword handler that allows fuzzy subkeyword matching.'''
-
+    min_ratio = 70
     help_text = u'Please send {keyword} *KEYWORD* to get help on *KEYWORD*'
+    subkeywords = []
 
     def handle(self, text):
-        pass
+        try:
+            parts = text.split(None, 1)
+        except ValueError:
+            self.respond(self.help_text)
+            return
+
+        if len(parts) == 1:
+            subkeyword = parts[0]
+            message = u''
+        else:
+            subkeyword, message = parts
+
+        result, ratio = process.extractOne(subkeyword, self.subkeywords)
+        if ratio < self.min_ratio:
+            self.help()
+            return
+
+        handler_name = u'handle_{}'.format(result)
+        handler = getattr(self, handler_name)
+
+        handler(message)
+
 
     def help(self):
         if self.help_text:
--- handlers/tests.py
+++ handlers/tests.py
@@ -7,10 +7,50 @@ from handlers.fuzzy import FuzzySubKeywordHandler
 class FuzzySubKeywordHandlerTest(TestCase):
     def setUp(self):
         FuzzySubKeywordHandler.keyword = u'TEST'
+        FuzzySubKeywordHandler.subkeywords = [u'first', u'second']
+
         self.help_response = u'Please send {} *KEYWORD* to get help on *KEYWORD*'.format(
             FuzzySubKeywordHandler.keyword)
 
+        self.canned_responses = {
+            keyword: u'This is the response for the "{}" subkeyword'.format(
+                keyword) for keyword in FuzzySubKeywordHandler.subkeywords
+        }
+
+        FuzzySubKeywordHandler.handle_first = lambda handler, text: handler.respond(
+            self.canned_responses[u'first'])
+        FuzzySubKeywordHandler.handle_second = lambda handler, text: handler.respond(
+            self.canned_responses[u'second'])
+
     def test_help_message(self):
         responses = FuzzySubKeywordHandler.test(FuzzySubKeywordHandler.keyword)
+        self.assertTrue(responses)
+        self.assertEqual(responses[0], self.help_response)
+
+        responses = FuzzySubKeywordHandler.test(
+            u'{} '.format(FuzzySubKeywordHandler.keyword))
+        self.assertEqual(responses[0], self.help_response)
+
+    def test_exact_keyword_match(self):
+        test_keyword = FuzzySubKeywordHandler.subkeywords[0]
+
+        responses = FuzzySubKeywordHandler.test(u'{} {}'.format(
+            FuzzySubKeywordHandler.keyword, test_keyword))
+        self.assertTrue(responses)
+        self.assertEqual(responses[0], self.canned_responses[test_keyword])
+
+    def test_good_inexact_keyword_match(self):
+        test_keyword = u'fir'
+
+        responses = FuzzySubKeywordHandler.test(u'{} {}'.format(
+            FuzzySubKeywordHandler.keyword, test_keyword))
+        self.assertTrue(responses)
+        self.assertEqual(responses[0], self.canned_responses[u'first'])
+
+    def test_poor_inexact_keyword_match(self):
+        test_keyword = u'fur'
+
+        responses = FuzzySubKeywordHandler.test(u'{} {}'.format(
+            FuzzySubKeywordHandler.keyword, test_keyword))
         self.assertTrue(responses)
         self.assertEqual(responses[0], self.help_response)
