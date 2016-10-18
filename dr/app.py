16f04031004baf8461be373c55eee866e59473db
Oladipo Odumosu <dodumosu@timbaobjects.com>
2016-10-18 13:04:44 +0100

    Initial commit of DR app

---
+++ dr/app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from rapidsms.apps.base import AppBase
+
+
+class DeathRegistrationApp(AppBase):
+    def parse(self, message):
+        pass
+
+    def handle(self, message):
+        pass
