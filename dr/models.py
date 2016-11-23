0ee13b008fd0a7180402d2b27b8130170698f72e
Oladipo Odumosu <dodumosu@timbaobjects.com>
2016-11-23 20:56:10 +0100

    Updated field map to match UNICEF's requirements

--- dr/models.py
+++ dr/models.py
@@ -6,42 +6,56 @@ from locations.models import Location
 from reporters.models import Reporter, PersistantConnection
 
 FIELD_MAP = {
-    u'AA': u'Number of certified deaths of males under 7 days old',
-    u'AB': u'Number of certified deaths of females under 7 days old',
-    u'AC': u'Number of uncertified deaths of males under 7 days old',
-    u'AD': u'Number of uncertified deaths of females under 7 days old',
-    u'BA': u'Number of certified deaths of males 7-28 days old',
-    u'BB': u'Number of certified deaths of females 7-28 days old',
-    u'BC': u'Number of uncertified deaths of males 7-28 days old',
-    u'BD': u'Number of uncertified deaths of females 7-28 days old',
-    u'CA': u'Number of certified deaths of males 1 to 12 months old',
-    u'CB': u'Number of certified deaths of females 1 to 12 months old',
-    u'CC': u'Number of uncertified deaths of males 1 to 12 months old',
-    u'CD': u'Number of uncertified deaths of females 1 to 12 months old',
-    u'DA': u'Number of certified deaths of males 1 to 4 years old',
-    u'DB': u'Number of certified deaths of females 1 to 4 years old',
-    u'DC': u'Number of uncertified deaths of males 1 to 4 years old',
-    u'DD': u'Number of uncertified deaths of females 1 to 4 years old',
-    u'EA': u'Number of certified deaths of males 5 to 14 years old',
-    u'EB': u'Number of certified deaths of females 5 to 14 years old',
-    u'EC': u'Number of uncertified deaths of males 5 to 14 years old',
-    u'ED': u'Number of uncertified deaths of females 5 to 14 years old',
-    u'FA': u'Number of certified deaths of males 15 to 24 years old',
-    u'FB': u'Number of certified deaths of females 15 to 24 years old',
-    u'FC': u'Number of uncertified deaths of males 15 to 24 years old',
-    u'FD': u'Number of uncertified deaths of females 15 to 24 years old',
-    u'GA': u'Number of certified deaths of males 25 to 44 years old',
-    u'GB': u'Number of certified deaths of females 25 to 44 years old',
-    u'GC': u'Number of uncertified deaths of males 25 to 44 years old',
-    u'GD': u'Number of uncertified deaths of males 25 to 44 years old',
-    u'HA': u'Number of certified deaths of males 45 to 64 years old',
-    u'HB': u'Number of certified deaths of females 45 to 64 years old',
-    u'HC': u'Number of uncertified deaths of males 45 to 64 years old',
-    u'HD': u'Number of uncertified deaths of females 45 to 64 years old',
-    u'JA': u'Number of certified deaths of males over 65 years old',
-    u'JB': u'Number of certified deaths of females over 65 years old',
-    u'JC': u'Number of uncertified deaths of males over 65 years old',
-    u'JD': u'Number of uncertified deaths of females over 65 years old',
+    u'AA': u'The number of medically certified female deaths due to childbirth and complications',
+    u'AB': u'The number of uncertified female deaths due to childbirth and other complicatons',
+    u'BA': u'The number of medically certified deaths of males under 1 due to fevers/typhoid',
+    u'BB': u'The number of uncertified deaths of males under 1 due to fevers/typhoid',
+    u'BC': u'The number of medically certified deaths of females under 1 due to fevers/typhoid',
+    u'BD': u'The number of uncertified deaths of females under 1 due to fevers/typhoid',
+    u'BE': u'The number of medically certified deaths of males 1-4 years old due to fevers/typhoid',
+    u'BF': u'The number of uncertified deaths of males 1-4 years old due to fevers/typhoid',
+    u'BG': u'The number of medically certified deaths of females 1-4 years old due to fevers/typhoid',
+    u'BH': u'The number of uncertified deaths of females 1-4 years old due to fevers/typhoid',
+    u'BJ': u'The number of medically certified deaths of males 5 years and older due to fevers/typhoid',
+    u'BK': u'The number of uncertified deaths of males 5 years and older due to fevers/typhoid',
+    u'BM': u'The number of medically certified deaths of females 5 years and older due to fevers/typhoid',
+    u'BN': u'The number of uncertified deaths of females 5 years and older due to fevers/typhoid',
+    u'CA': u'The number of medically certified deaths of males under 1 due to accidents and injuries',
+    u'CB': u'The number of uncertified deaths of males under 1 due to accidents and injuries',
+    u'CC': u'The number of medically certified deaths of females under 1 due to accidents and injuries',
+    u'CD': u'The number of uncertified deaths of females under 1 due to accidents and injuries',
+    u'CE': u'The number of medically certified deaths of males 1-4 years old due to accidents and injuries',
+    u'CF': u'The number of uncertified deaths of males 1-4 years old due to accidents and injuries',
+    u'CG': u'The number of medically certified deaths of females 1-4 years old due to accidents and injuries',
+    u'CH': u'The number of uncertified deaths of females 1-4 years old due to accidents and injuries',
+    u'CJ': u'The number of medically certified deaths of males 5 years and older due to accidents and injuries',
+    u'CK': u'The number of uncertified deaths of males 5 years and older due to accidents and injuries',
+    u'CM': u'The number of medically certified deaths of females 5 years and older due to accidents and injuries',
+    u'CN': u'The number of uncertified deaths of females 5 years and older due to accidents and injuries',
+    u'DA': u'The number of medically certified deaths of males under 1 due to HIV/AIDS',
+    u'DB': u'The number of uncertified deaths of males under 1 due to HIV/AIDS',
+    u'DC': u'The number of medically certified deaths of females under 1 due to HIV/AIDS',
+    u'DD': u'The number of uncertified deaths of females under 1 due to HIV/AIDS',
+    u'DE': u'The number of medically certified deaths of males 1-4 years old due to HIV/AIDS',
+    u'DF': u'The number of uncertified deaths of males 1-4 years old due to HIV/AIDS',
+    u'DG': u'The number of medically certified deaths of females 1-4 years old due to HIV/AIDS',
+    u'DH': u'The number of uncertified deaths of females 1-4 years old due to HIV/AIDS',
+    u'DJ': u'The number of medically certified deaths of males 5 years and older due to HIV/AIDS',
+    u'DK': u'The number of uncertified deaths of males 5 years and older due to HIV/AIDS',
+    u'DM': u'The number of medically certified deaths of females 5 years and older due to HIV/AIDS',
+    u'DN': u'The number of uncertified deaths of females 5 years and older due to HIV/AIDS',
+    u'EA': u'The number of medically certified deaths of males under 1 due to other ailments',
+    u'EB': u'The number of uncertified deaths of males under 1 due to other ailments',
+    u'EC': u'The number of medically certified deaths of females under 1 due to other ailments',
+    u'ED': u'The number of uncertified deaths of females under 1 due to other ailments',
+    u'EE': u'The number of medically certified deaths of males 1-4 years old due to other ailments',
+    u'EF': u'The number of uncertified deaths of males 1-4 years old due to other ailments',
+    u'EG': u'The number of medically certified deaths of females 1-4 years old due to other ailments',
+    u'EH': u'The number of uncertified deaths of females 1-4 years old due to other ailments',
+    u'EJ': u'The number of medically certified deaths of males 5 years and older due to other ailments',
+    u'EK': u'The number of uncertified deaths of males 5 years and older due to other ailments',
+    u'EM': u'The number of medically certified deaths of females 5 years and older due to other ailments',
+    u'EN': u'The number of uncertified deaths of females 5 years and older due to other ailments',
 }
 
 
@@ -50,17 +64,12 @@ class DeathReport(models.Model):
 	Represents a single death record.
 
 	Each death record contains the number of certified and uncertified
-	deaths, male and female, for each of the age groups below:
-	- under 7D
-	- 7 - 28D
-	- 1 - 12M
-	- 1 - 4Y
-	- 5 - 14Y
-	- 15 - 24Y
-	- 25 - 44Y
-	- 45 - 64Y
-	- 65Y+
-	...bringing this to a total of 36 individual data points per record.
+	deaths, male and female, for each cause of death, for each of the age groups below:
+	- under 1y
+	- 1-4y
+	- 5+y
+	...bringing this to a total of 50 individual data points per record (only
+    females over 5 can die of childbirth and pregnancy complications)
 	For this reason, we're using a JSONField (which requires MySQL 5.7+)
 	'''
 	location = models.ForeignKey(Location, related_name=u'death_reports')
