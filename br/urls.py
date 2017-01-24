02a98645ff9455b7ef87671b9a82a48beb2ba256
Tim Akinbo <takinbo@timbaobjects.com>
2017-01-24 14:40:28 +0100

    fix url prefixes

--- br/urls.py
+++ br/urls.py
@@ -6,15 +6,15 @@ from br.views import *
 from locations.api import LocationItemView, TypedLocationListView
 
 urlpatterns = [
-    #url(r'^br/?(?P<prefix>(monthly)?)/?(?P<state>\d*)/?(?P<year>\d*)/?(?P<month>\d*)/?$', 'unicefng.br.views.dashboard'),
-    #url(r'^br/data/?(?P<prefix>(monthly)?)/?(?P<state>\d*)/?(?P<year>\d*)/?(?P<month>\d*)/?$', 'unicefng.br.views.csv_download'),
-    url(r'^br/api/locations', TypedLocationListView.as_view(), name='api_location_list'),
-    url(r'^br/api/location/(?P<pk>[0-9]+)/?$', LocationItemView.as_view(), name='api_location_detail'),
-    url(r'^br/faq/?$', FAQView.as_view(), name='faq'),
-    url(r'^br/reports/?$', ReportListView.as_view(), name='reports_list'),
-    url(r'^br/report/(?P<pk>\d+)/?$', ReportEditView.as_view(),
+    #url(r'^(?P<prefix>(monthly)?)/?(?P<state>\d*)/?(?P<year>\d*)/?(?P<month>\d*)/?$', 'unicefng.br.views.dashboard'),
+    #url(r'^data/?(?P<prefix>(monthly)?)/?(?P<state>\d*)/?(?P<year>\d*)/?(?P<month>\d*)/?$', 'unicefng.br.views.csv_download'),
+    url(r'^api/locations', TypedLocationListView.as_view(), name='api_location_list'),
+    url(r'^api/location/(?P<pk>[0-9]+)/?$', LocationItemView.as_view(), name='api_location_detail'),
+    url(r'^faq/?$', FAQView.as_view(), name='faq'),
+    url(r'^reports/?$', ReportListView.as_view(), name='reports_list'),
+    url(r'^report/(?P<pk>\d+)/?$', ReportEditView.as_view(),
         name='report_edit'),
-    url(r'^br/report/(?P<pk>\d+)/delete$', ReportDeleteView.as_view(),
+    url(r'^report/(?P<pk>\d+)/delete$', ReportDeleteView.as_view(),
         name='report_delete'),
     url(r'^/?$', dashboardview, name='dashboard'),
     url(r'^(?P<year>\d+)/?$', dashboardview, name='dashboard_with_year'),
@@ -26,9 +26,9 @@ urlpatterns = [
         name='dashboard_with_state_and_year'),
     url(r'^(?P<state>[a-z\-]+)/(?P<year>\d+)/(?P<month>\d+)/?$', dashboardview,
         name='dashboard_with_state_year_and_month'),
-    url(r'^br/center/new/?$', CenterCreationView.as_view(), name='center_add'),
-    url(r'^br/centers/?$', CenterListView.as_view(), name='center_list'),
-    url(r'^br/center/(?P<pk>\d+)/?$', CenterUpdateView.as_view(), name='center_edit'),
+    url(r'^center/new/?$', CenterCreationView.as_view(), name='center_add'),
+    url(r'^centers/?$', CenterListView.as_view(), name='center_list'),
+    url(r'^center/(?P<pk>\d+)/?$', CenterUpdateView.as_view(), name='center_edit'),
 ]
 
 # authentication urls
