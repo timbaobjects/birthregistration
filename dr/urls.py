ba05dbdab68f71b873b2e36f5d6199d0251d38a5
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-02-02 12:11:33 +0100

    Initial commit of DR edit template

--- dr/urls.py
+++ dr/urls.py
@@ -4,4 +4,6 @@ from dr import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'reports/(?P<pk>\d+)/?$', views.DeathReportUpdateView.as_view(),
+    	name=u'report_edit'),
 ]
