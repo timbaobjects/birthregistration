ce6e1be476095af5e285168a81b0ca2d79346055
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-02-17 15:06:59 +0100

    Added api package

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from locations import api as locations_api
+
+urlpatterns = [
+    url(r'^locations/(?P<pk>\d+)/?$', locations_api.LocationItemView.as_view(),
+        name=u'location_detail'),
+    url(r'^locations/?$', locations_api.LocationListView.as_view(),
+        name=u'location_list'),
+]
