9613029a4ab01b92d727d0714bff8344b7195fc8
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-02-17 15:07:52 +0100

    Added API urls to global urlconf

--- unicefng/urls.py
+++ unicefng/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     url(r'^selectable/', include('selectable.urls')),
     url(r'^br/', include('br.urls')),
     url(r'^dr/', include('dr.urls')),
+    url(r'^api/', include('api.urls', namespace=u'api')),
     url(r'incoming/', HttpBackendView.as_view(backend_name='polling')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
