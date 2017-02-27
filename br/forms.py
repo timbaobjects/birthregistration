fd8f84955c01c2ae662358dedf380689051c6d9c
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-02-27 15:37:02 +0100

    Fixed typo

--- br/forms.py
+++ br/forms.py
@@ -7,3 +7,9 @@ class BirthRegistrationModelForm(forms.ModelForm):
         model = BirthRegistration
         fields = ('girls_below1', 'girls_1to4', 'girls_5to9', 'girls_10to18',
                   'boys_below1', 'boys_1to4', 'boys_5to9', 'boys_10to18')
+
+
+class ReportDeleteForm(forms.Form):
+    reports = forms.ModelMultipleChoiceField(
+        queryset=BirthRegistration.objects.all(),
+        required=False, widget=forms.CheckboxSelectMultiple)
