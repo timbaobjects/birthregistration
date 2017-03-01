973adfd6809e5a63dda8c9ed57ea97d04a9cc47d
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-01 13:32:04 +0100

    Changed campaigns urlconf

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -2,8 +2,12 @@
 from django.conf.urls import url
 
 from campaigns import views
+from ipd import views as ipd_views
 
 urlpatterns = [
-	url(r'^$', views.CampaignListView.as_view(), name=u'campaign_list'),
-	url(r'^new/?$', views.CampaignCreateView.as_view(), name=u'campaign_new'),
+	url(r'^$', ipd_views.dashboard, name=u'dashboard'),
+	url(r'^(?P<campaign_id>\d+)/(?P<location_id>\d+)/?$', ipd_views.dashboard,
+		name=u'dashboard_detail'),
+	url(r'^campaigns/?$', views.CampaignListView.as_view(), name=u'campaign_list'),
+	url(r'^campaigns/new/?$', views.CampaignCreateView.as_view(), name=u'campaign_new'),
 ]
