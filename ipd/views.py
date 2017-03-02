6debfe13638877db16262cc8ffec43cfbb806e5d
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-02 11:49:51 +0100

    Changes to dashboard

--- ipd/views.py
+++ ipd/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.db.models import F
+from collections import OrderedDict
+
+from django.db.models import F, Sum
 from django.shortcuts import get_object_or_404, render
+from django_pandas.io import read_frame
 
 from campaigns.models import Campaign
 from ipd.models import Report
@@ -9,6 +12,8 @@ from locations.models import Location
 
 def dashboard(request, campaign_id=None, location_id=None):
     context = {}
+    lgas = None
+    lga_data = {}
 
     if campaign_id and location_id:
         campaign = get_object_or_404(Campaign, pk=campaign_id)
@@ -27,7 +32,26 @@ def dashboard(request, campaign_id=None, location_id=None):
         ).filter(loc_type='State').values(
         u'name', u'loc_name', u'loc_type', u'loc_pk', u'pk')
 
+    if campaign:
+        reports = campaign.get_related_objects(Report, location).annotate(
+            lga=F(u'location__parent__name'), ward=F(u'location__name'))
+        # dataframe = read_frame(reports, [u'immunized', u'commodity', u'lga', u'ward'], index_col=u'time')
+        lgas = campaign.campaign_lgas(location).order_by(u'name')
+
+        for lga in lgas:
+            the_data = [
+                reports.filter(lga=lga.name,
+                    commodity=code).aggregate(total=Sum(u'immunized')).get(u'total', None)
+                for code, description in Report.IM_COMMODITIES
+                # dataframe[lga == lga.name && commodity == code][u'immunized'].sum()
+                # for code, description in Report.IM_COMMODITIES
+            ]
+            lga_data[lga.name] = the_data
+
     context[u'campaigns'] = campaign_qs
+    context[u'lgas'] = lgas
+    context[u'commodities'] = OrderedDict(Report.IM_COMMODITIES)
+    context[u'vaccination_data'] = lga_data
     context[u'selected_campaign'] = campaign
     context[u'selected_location'] = location
 
