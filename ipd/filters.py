e844a42ef2deb10ffe313a2866bffbd93e0a085a
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-16 16:50:14 +0100

    Changed filter from using a date range filter to explicit start and end date filters

--- ipd/filters.py
+++ ipd/filters.py
@@ -8,4 +8,5 @@ from locations.models import Location
 class CampaignRelatedFilter(django_filters.FilterSet):
 	location = LocationFilter(queryset=Location.objects.filter(
 		type__name__in=[u'State', u'LGA']))
-	time = django_filters.DateFromToRangeFilter()
+	start_date = django_filters.DateFilter(u'time', lookup_type=u'gte')
+	end_date = django_filters.DateFilter(u'time', lookup_type=u'lte')
