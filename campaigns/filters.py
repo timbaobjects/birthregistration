501c345b2e5f897b5b84df0ef5540641db50436e
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-16 15:03:36 +0100

    Added filter for MNCHW campaigns list

---
+++ campaigns/filters.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import django_filters
+
+from locations.models import Location
+
+
+class CampaignLocationFilter(django_filters.ModelChoiceFilter):
+    def filter(self, qs, location):
+        if location:
+            return qs.filter(locations__pk=location.pk)
+
+        return qs
+
+
+class CampaignFilterSet(django_filters.FilterSet):
+    location = CampaignLocationFilter(queryset=Location.objects.filter(
+        type__name=u'State').order_by(u'name'))
+    start_date = django_filters.DateFilter(lookup_type=u'lte')
+    end_date = django_filters.DateFilter(lookup_type=u'gte')
