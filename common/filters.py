641b88766114fb65b8c111b19f9505daf7e66765
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-28 07:53:12 +0100

    Refactored common location filter to take advantage of `SearchableLocationQuerySet`

--- common/filters.py
+++ common/filters.py
@@ -5,7 +5,6 @@ import django_filters
 class LocationFilter(django_filters.ModelChoiceFilter):
     def filter(self, qs, location):
         if location:
-            descendant_locs = location.get_descendants(include_self=True)
-            return qs.filter(location__in=descendant_locs)
+            return qs.is_within(location)
 
         return qs
