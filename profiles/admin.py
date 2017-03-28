d2e8ed2df6570dd3e9de0f05968657d426b04873
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-28 08:37:20 +0100

    Added explicit admin support for Profile model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from profiles.forms import ProfileAdminForm
+from profiles.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileAdminForm
+    list_display = (u'user',)
+    search_fields = (u'user__username',)
+
+
+admin.site.register(Profile, ProfileAdmin)
