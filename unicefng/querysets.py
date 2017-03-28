ebbafe89c3f02eebdcb88994870b906efcd26053
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-03-28 07:39:01 +0100

    Added django-mysql's `QuerySetMixin` to `SearchableLocationQuerySet`

--- unicefng/querysets.py
+++ unicefng/querysets.py
@@ -1,7 +1,23 @@
-from django.db.models.query import QuerySet
+from django.db.models.query import Q, QuerySet
+from django_mysql.models import QuerySetMixin
+
+from profiles.models import Profile
 
 
-class SearchableLocationQuerySet(QuerySet):
+class SearchableLocationQuerySet(QuerySetMixin, QuerySet):
     def is_within(self, location):
         return self.filter(
             location__in=location.get_descendants(include_self=True))
+
+    def filter_supervised_locations(self, user):
+        try:
+            profile = user.profile
+        except Profile.DoesNotExist:
+            return self
+
+        query = Q()
+        for location in profile.locations.all():
+            query = query | Q(location__in=location.get_descendants(
+                include_self=True))
+
+        return self.filter(query)
