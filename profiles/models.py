a1fbb5c7fa2c9662b8a9f09495d4f6e07db9222f
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-04-08 10:53:24 +0100

    Added `filter_locations` method to `Profile` model.

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from operator import or_
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import six
 
 from locations.models import Location
 
@@ -8,3 +11,10 @@ from locations.models import Location
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name=u'profile')
     locations = models.ManyToManyField(Location, related_name=u'+')
+
+    def filter_locations(self, queryset):
+        query = (models.Q(pk__in=location.get_descendants(include_self=True))
+                    for location in self.locations.all())
+        query = six.functools.reduce(or_, query)
+
+        return queryset.filter(query)
