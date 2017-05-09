c3c9a3dee9027457f6e47a2028c123cc81f5fe7c
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-05-09 13:18:38 +0100

    Added utitilies module to common package

---
+++ common/utilities.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from reporters.models import PersistantConnection, Reporter
+
+
+def getConnectionAndReporter(message, permitted_role_codes):
+	connection = PersistantConnection.from_message(message)
+	reporter = connection.reporters.filter(
+		role__code__in=permitted_role_codes).order_by(u'-pk').first()
+
+	return connection, reporter
