a207da912dad45c2772cc86fb28f36e45b95bf60
Oladipo Odumosu <dodumosu@timbaobjects.com>
2017-05-23 12:57:00 +0100

    Updated form type and template

--- campaigns/forms.py
+++ campaigns/forms.py
@@ -2,9 +2,14 @@
 from django import forms
 
 from campaigns.models import Campaign
+from locations.models import Location
 
 
 class CampaignCreateForm(forms.ModelForm):
+    locations = forms.ModelMultipleChoiceField(
+        queryset=Location.objects.filter(type__name__in=[u'LGA', u'State']),
+        widget=forms.CheckboxSelectMultiple())
+
     class Meta:
         model = Campaign
         fields = (u'name', u'locations', u'start_date', u'end_date')
