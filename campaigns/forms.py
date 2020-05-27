bb2842fdd7796ff541ff8db070031f370506282f
Odumosu Dipo <dodumosu@timbaobjects.com>
2020-05-27 06:54:52 +0100

    feat: incorporate changes from live

--- campaigns/forms.py
+++ campaigns/forms.py
@@ -5,14 +5,25 @@ from campaigns.models import Campaign
 from locations.models import Location
 
 
-class CampaignForm(forms.ModelForm):
+class CampaignCreateForm(forms.ModelForm):
+    program = forms.ChoiceField(
+        choices=Campaign.PROGRAM_TYPES.items(),
+        widget=forms.widgets.RadioSelect)
     locations = forms.ModelMultipleChoiceField(
         queryset=Location.objects.filter(type__name__in=[u'LGA', u'State']),
         widget=forms.CheckboxSelectMultiple())
-
     start_date = forms.DateField(input_formats=['%d/%m/%Y'])
     end_date = forms.DateField(input_formats=['%d/%m/%Y'])
 
     class Meta:
         model = Campaign
-        fields = (u'name', u'locations', u'start_date', u'end_date')
+        fields = (u'locations', u'start_date', u'end_date')
+
+
+class CampaignUpdateForm(forms.ModelForm):
+    start_date = forms.DateField(input_formats=['%d/%m/%Y'])
+    end_date = forms.DateField(input_formats=['%d/%m/%Y'])
+
+    class Meta:
+        model = Campaign
+        fields = (u'locations', u'start_date', u'end_date')
