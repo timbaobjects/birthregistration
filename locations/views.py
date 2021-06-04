d7e75fc906966b22be2aa345e5347c32b028a4c7
Odumosu Dipo <dodumosu@timbaobjects.com>
2021-06-04 14:24:17 +0100

    feat: add view for nonreporting centres
    also, fix issues with centre edit view+template

--- locations/views.py
+++ locations/views.py
@@ -1,4 +1,5 @@
 # vim: ai ts=4 sts=4 et sw=4
+import csv
 import json
 
 from django.contrib import messages
@@ -7,17 +8,23 @@ from django.contrib.auth.mixins import (
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.db import connection
+from django.db.models import (
+    Case, Count, IntegerField, OuterRef, Subquery, When)
 from django.forms.formsets import formset_factory
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import (
+    HttpResponse, HttpResponseRedirect, StreamingHttpResponse)
 from django.shortcuts import get_object_or_404
+from django.utils.timezone import now
 from django.views.generic import ListView, FormView, TemplateView
 from drf_yasg.utils import swagger_auto_schema
 import pandas as pd
 
 from django.conf import settings
 
+from br.models import BirthRegistration
 from locations import raw_queries
-from locations.forms import generate_edit_form, CenterCreationForm
+from locations.forms import (
+    generate_edit_form, CenterCreationForm, NonReportingCentresFilterForm)
 from locations.filters import CenterFilterSet
 from locations.models import Facility, Location, LocationType
 
@@ -57,10 +64,13 @@ class CenterListView(LoginRequiredMixin, ListView):
 
 class CenterUpdateView(
         LoginRequiredMixin, PermissionRequiredMixin, FormView):
+    permission_required = 'locations.edit_location'
     template_name = 'locations/center_edit.html'
 
     def get_context_data(self, **kwargs):
         context = super(CenterUpdateView, self).get_context_data(**kwargs)
+
+        self.object = self.get_object()
 
         context['page_title'] = 'Edit center: {}'.format(self.object.name)
         context[u'location'] = self.object
@@ -77,8 +87,7 @@ class CenterUpdateView(
         center = Location.objects.get(pk=form.cleaned_data['id'])
         center.name = form.cleaned_data['name']
         center.code = form.cleaned_data['code']
-        center.parent = Location.objects.get_object_or_404(
-            pk=form.cleaned_data['lga'])
+        center.parent = get_object_or_404(Location, pk=form.cleaned_data['lga'])
         center.active = form.cleaned_data['active']
         center.save()
         center.facilities.update(
@@ -87,14 +96,14 @@ class CenterUpdateView(
 
         return HttpResponseRedirect(self.get_success_url())
 
-    def get_form(self, form_class):
-        return generate_edit_form(self.object)
+    def get_form(self, form_class=None):
+        return generate_edit_form(self.get_object())
 
     def get_queryset(self):
         return Location.objects.filter(type__name='RC')
 
     def get_success_url(self):
-        return reverse('center_list')
+        return reverse('locations:center_list')
 
     def post(self, request, *args, **kwargs):
         form = generate_edit_form(self.get_object(), request.POST)
@@ -195,3 +204,86 @@ def facilities(request):
     facility_dataframe.to_csv(response, encoding='UTF-8', index=False)
 
     return response
+
+
+
+class CSVBuffer(object):
+    def write(self, value):
+        return value
+
+
+def _get_rows(centres):
+    yield ['Centre', 'Code', 'LGA', 'State', 'Active?', 'Last report date']
+
+    for centre in centres:
+        last_report_time = centre.latest_birth_report_time()
+        yield [
+            centre.name,
+            centre.code,
+            centre.parent.name,
+            centre.parent.parent.name,
+            'Yes' if centre.active else 'No',
+            last_report_time.strftime('%d-%m-%Y') if last_report_time else 'N/A'
+        ]
+
+
+class NonReportingCentresView(LoginRequiredMixin, ListView):
+    context_object_name = 'centres'
+    page_title = 'Non-reporting centres'
+    paginate_by = settings.PAGE_SIZE
+    template_name = 'locations/non_reporting_centre_list.html'
+
+    def get(self, request, *args, **kwargs):
+        centres = self.get_queryset()
+
+        year = request.GET.get('year') or None
+        month = request.GET.get('month') or None
+
+        if request.GET.get('export'):
+            writer = csv.writer(CSVBuffer())
+            response = StreamingHttpResponse((writer.writerow(row) for row in _get_rows(centres)), content_type='text/csv')
+            filename = 'non-reporting-centres-{}-{}.csv'.format(
+                year,
+                str(month).zfill(2)
+            )
+            response['Content-Disposition'] = 'attachment; filename={}'.format(
+                filename)
+            
+            return response
+        
+        return super(NonReportingCentresView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(NonReportingCentresView, self).get_context_data(**kwargs)
+
+        context['page_title'] = self.page_title
+        context['filter_form'] = self.filter_form
+        context['year'] = self.request.GET.get('year') or now().year
+        context['month'] = self.request.GET.get('month') or ''
+
+        return context
+
+    def get_queryset(self):
+        self.filter_form = NonReportingCentresFilterForm(self.request.GET)
+        if self.filter_form.is_valid():
+            filter_data = self.filter_form.cleaned_data
+        else:
+            filter_data = {}
+
+        filter_kwargs = {}
+        year = filter_data.get('year') or now().year
+        filter_kwargs['birthregistration_records__time__year'] = int(year)
+        month = filter_data.get('month')
+        if month:
+            filter_kwargs['birthregistration_records__time__month'] = int(month)
+
+        if len(filter_kwargs) == 0:
+            centres = Location.objects.none()
+        else:
+            centres = Location.objects.filter(
+                type__name='RC'
+            ).annotate(
+                cnt=Count(Case(When(then=1, **filter_kwargs)))
+            ).filter(cnt=0)
+
+        return centres
