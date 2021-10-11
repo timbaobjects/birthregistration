b31e1189cd196e9e4fb4a838f748eedef472a796
Odumosu Dipo <dodumosu@timbaobjects.com>
2021-10-11 19:56:31 +0100

    feat: document query parameters

--- vitalregpro/api/views/br.py
+++ vitalregpro/api/views/br.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
 from django.http import JsonResponse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
+from rest_framework.decorators import api_view
 
 from vitalregpro.api import serializers
 from vitalregpro.models import BirthRegistration, Location, LocationType
@@ -17,6 +20,8 @@ def _default_queryset():
     return queryset
 
 
+@swagger_auto_schema(method='get')
+@api_view()
 def br_location_types(request):
     return JsonResponse({
         'types': BR_LOCATION_TYPE_NAMES
@@ -28,8 +33,24 @@ class LocationItemView(generics.RetrieveAPIView):
     serializer_class = serializers.LocationSerializer
 
 
+list_type_param = openapi.Parameter(
+    'type', openapi.IN_QUERY, description='Location type',
+    type=openapi.TYPE_STRING)
+list_in_param = openapi.Parameter(
+    'in', openapi.IN_QUERY, description='Ancestor location code',
+    type=openapi.TYPE_STRING)
+list_month_param = openapi.Parameter(
+    'month', openapi.IN_QUERY, description='Month', type=openapi.TYPE_INTEGER)
+list_year_param = openapi.Parameter(
+    'year', openapi.IN_QUERY, description='Year', type=openapi.TYPE_INTEGER)
+
+
 class LocationListView(generics.ListAPIView):
     serializer_class = serializers.LocationSerializer
+
+    @swagger_auto_schema(manual_parameters=[list_in_param, list_type_param,])
+    def get(self, request, *args, **kwargs):
+        return super(LocationListView, self).get(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = _default_queryset()
@@ -58,6 +79,11 @@ class BirthReportListCreateView(generics.ListCreateAPIView):
     queryset = BirthRegistration.objects.order_by('-time')
     serializer_class = serializers.BirthReportSerializer
 
+    @swagger_auto_schema(
+        manual_parameters=[list_in_param, list_month_param, list_year_param])
+    def get(self, request, *args, **kwargs):
+        return super(BirthReportListCreateView, self).get(request, *args, **kwargs)
+
     def get_queryset(self):
         ancestor_code = self.request.query_params.get('in')
         month = self.request.query_params.get('month')
@@ -82,4 +108,3 @@ class BirthReportListCreateView(generics.ListCreateAPIView):
             )
 
         return queryset
-
