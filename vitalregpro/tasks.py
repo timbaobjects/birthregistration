edc6305a0f96a40a7fa56064469f8082c874bd9b
Odumosu Dipo <dodumosu@timbaobjects.com>
2022-05-31 21:56:48 +0100

    fix: add missing import

--- vitalregpro/tasks.py
+++ vitalregpro/tasks.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import logging
 import re
 
+from dateutil.relativedelta import relativedelta
 from celery import shared_task
 from django.utils.timezone import get_current_timezone, now
 from fuzzywuzzy import process
