64ccc2294437a92434951fc0518f1459877c2218
Odumosu Dipo <dodumosu@timbaobjects.com>
2022-06-02 21:15:14 +0100

    feat: update centre add template

--- locations/views.py
+++ locations/views.py
@@ -57,13 +57,15 @@ class CenterListView(LoginRequiredMixin, ListView):
 
 class CenterUpdateView(
         LoginRequiredMixin, PermissionRequiredMixin, FormView):
-    template_name = 'locations/center_edit.html'
+    permission_required = 'locations.add_location'
+    template_name = 'backend/centre_edit.html'
 
     def get_context_data(self, **kwargs):
+        instance = self.get_object()
         context = super(CenterUpdateView, self).get_context_data(**kwargs)
 
-        context['page_title'] = 'Edit center: {}'.format(self.object.name)
-        context[u'location'] = self.object
+        context['page_title'] = 'Edit center: {}'.format(instance.name)
+        context[u'location'] = instance
 
         return context
 
@@ -73,43 +75,35 @@ class CenterUpdateView(
 
         return location
 
-    def form_valid(self, form):
-        center = Location.objects.get(pk=form.cleaned_data['id'])
-        center.name = form.cleaned_data['name']
-        center.code = form.cleaned_data['code']
-        center.parent = Location.objects.get_object_or_404(
-            pk=form.cleaned_data['lga'])
-        center.active = form.cleaned_data['active']
-        center.save()
-        center.facilities.update(
-            code=center.code, name=center.name
-        )
-
-        return HttpResponseRedirect(self.get_success_url())
-
-    def get_form(self, form_class):
-        return generate_edit_form(self.object)
+    def get_form(self, form_class=None):
+        return generate_edit_form(self.get_object())
 
     def get_queryset(self):
         return Location.objects.filter(type__name='RC')
 
     def get_success_url(self):
-        return reverse('center_list')
+        return reverse('locations:center_list')
 
     def post(self, request, *args, **kwargs):
         form = generate_edit_form(self.get_object(), request.POST)
 
         if form.is_valid():
-            return self.form_valid(form)
+            instance = self.get_object()
+            instance.name = form.cleaned_data.get('name')
+            instance.active = form.cleaned_data.get('active')
+            instance.save()
+
+            return HttpResponseRedirect(self.get_success_url())
+            
         else:
             return self.form_invalid(form)
 
 
 class CenterCreationView(
         LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
-    page_title = u'Create registration centers'
+    page_title = u'Create registration centres'
     permission_required = 'locations.add_location'
-    template_name = u'locations/center_new.html'
+    template_name = u'backend/centre_new.html'
 
     def get_context_data(self, **kwargs):
 
